193a2e92db68942576dbfdb33b6adbd6f37f425d
antwave <antonio.carrizosa@outlook.com>
2024-01-26 18:38:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import markdownSample from "./assets/sampleMarkdown.json";
+import Markdown from "react-markdown";
+
+function App() {
+  const sample = markdownSample[0].md;
+  const [md, setMD] = useState<string>(sample);
+  return (
+    <>
+      <div id="background">
+        <h1>ANT.WAV's Markdown Previewer</h1>
+
+        <div className="window">
+          <label htmlFor="editor">Type your Makrdown here!</label>
+          <textarea
+            name="Editor"
+            id="editor"
+            value={md}
+            onChange={(e) => setMD(e.target.value)}
+          ></textarea>
+        </div>
+        <div className="window">
+          <label htmlFor="preview">Rendered Markdown</label>
+          <div id="preview">
+            <Markdown>{md}</Markdown>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
